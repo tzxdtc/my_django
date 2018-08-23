81f6b8745186ea2e4dcb5fb6df81530b3bb4ae87
tan <tzxdtc10@gmail.com>
2018-08-23 22:33:21 +0900

    管理画面の準備

--- diary/models.py
+++ diary/models.py
@@ -6,3 +6,6 @@ class Day(models.Model):
     title = models.CharField('タイトル', max_length=200)
     text = models.TextField('本文')
     date = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return self.title
