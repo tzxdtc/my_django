365a50da7bd94bd46e49c65405e1802cc67445a7
tan <tzxdtc10@gmail.com>
2018-08-26 14:07:26 +0900

    make some diff on models

--- project2/employee/models.py
+++ project2/employee/models.py
@@ -9,12 +9,22 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
+class Club(models.Model):
+    name = models.CharField('club',max_length=20)
+    created_at = models.DateTimeField('date',default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     first_name = models.CharField('FirstName',max_length=20)
     last_name = models.CharField('LastName',max_length=20)
     email = models.EmailField('email',blank=True)
     department = models.ForeignKey(
         Department, verbose_name='department',on_delete=models.PROTECT,
+    )
+    club = models.ManyToManyField(
+        Club,verbose_name='club',
     )
     created_at = models.DateTimeField('date',default=timezone.now)
 
